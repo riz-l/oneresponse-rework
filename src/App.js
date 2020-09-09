749528ce07e62d27944abb5838ee292b801aacf7
Riz <joshua.riz.layton@gmail.com>
2020-09-09 17:36:56 +0100

    Adding starter files including react-router-dom, node-sass and styled-components packages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import: Dependencies
+import React from "react";
 
+// Import: Styling
+import "./App.styles.scss";
+
+// Component: App
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>One Response</h1>
     </div>
   );
 }
 
+// Export: App
 export default App;
