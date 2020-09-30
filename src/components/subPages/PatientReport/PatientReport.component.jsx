4d6edbc018499eedcfb4688f8ee0e22360d4fd69
Riz <joshua.riz.layton@gmail.com>
2020-09-30 12:21:40 +0100

    Added initial render animation

--- src/components/subPages/PatientReport/PatientReport.component.jsx
+++ src/components/subPages/PatientReport/PatientReport.component.jsx
@@ -2541,7 +2541,7 @@ const PatientReportContainer = styled.div`
   margin: 1rem;
 `;
 
-// Styled: fsadeIn
+// Styled: fadeIn
 const fadeIn = keyframes`
     0% {opacity: 0; transform: translateY(-20px)};
     100% {opacity: 1; transform: translateY(0)};
